1b7cd562e870c338ced247a8eaffedbd298057ec
Voleking <volekingsg@gmail.com>
2017-12-12 08:52:46 +0800

    Add index & models

---
+++ LMS/management/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# admin.register(Profile)
+# admin.register(Book)
+# admin.register(Borrow)
+# admin.register(BookCategory)
+# admin.register(MemberLevel)
+# admin.register(LossReporting)
